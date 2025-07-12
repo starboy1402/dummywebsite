ac00da872f64a1fe6643c1a532ab4ae5a3bcdc97
alif hasan <alifsakib1@gmail.com>
2025-07-12 21:52:22 +0600

    added

--- supabase/init.js
+++ supabase/init.js
@@ -2,5 +2,5 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js'
 
 const supabaseUrl = 'https://rbcjwbluvmjbkxgatzgw.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJiY2p3Ymx1dm1qYmt4Z2F0emd3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIyNTA3NTcsImV4cCI6MjA2NzgyNjc1N30.uc5vgyJP-kgR4OxD9AOaenA3fq0fg12dngQ5zoYQoLk'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJiY2p3Ymx1dm1qYmt4Z2F0emd3Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1MjI1MDc1NywiZXhwIjoyMDY3ODI2NzU3fQ.XJ1X4hAwzTYaeQqqYRJHuVhS0MIyloU67GmIWrPvAF4'
 export const supabase = createClient(supabaseUrl, supabaseKey)
